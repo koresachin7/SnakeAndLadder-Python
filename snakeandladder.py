f151af61b44508ae6d67614af66912d4f0c48910
koresachin7 <koresachin7@gmail.com>
2021-12-04 14:07:51 +0530

    Added : - Creating method playerOption and generat random number between 0 to 2 also check that using if and elif ladder

--- snakeandladder.py
+++ snakeandladder.py
@@ -21,10 +21,30 @@ class SnakeAndLadder:
         print("Dice Roll is : -", dice)
         return dice
 
+    def player_option(self):
+        """
+        Description:
+            call roll_dice method to update position
+            use Random  to generating number between 0 to 2
+            use if and elif ladder to check player option
+        """
+        dice = snake_and_ladder.roll_dice()
+        option = random.randint(0, 2)
+        print("Player Roll Option is : -", option)
+        print("0.no Player 1. Ladder 2.Snake")
+        if option == 0:
+            print("no Player")
+        elif option == 1:
+            print("Ladder")
+            self.position += dice
+        elif option == 2:
+            print("Snake")
+            self.position -= dice
+        else:
+            print("System error")
+
 
 if __name__ == '__main__':
     print("Welcome to Snake and Ladder Problem")
     snake_and_ladder = SnakeAndLadder()
-    # calling function her and store method return value
-    dice_value = snake_and_ladder.roll_dice()
-    print(dice_value)
+    snake_and_ladder.player_option()
