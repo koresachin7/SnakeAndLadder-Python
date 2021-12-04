269371708e2804d5b126a23cc18a364112c8fd25
koresachin7 <koresachin7@gmail.com>
2021-12-04 13:24:15 +0530

    Added : - Welcome message

---
+++ snakeandladder.py
@@ -0,0 +1,13 @@
+"""
+* @Author: Sachin S Kore
+* @Date: 2021-12-4
+* @Title :  To print Snake And Ladder Welcome message
+"""
+
+
+class SnakeAndLadder:
+    pass
+
+
+if __name__ == '__main__':
+    print("Welcome to Snake and Ladder Problem")
