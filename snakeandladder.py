1f0dbd639af13934364f33ce4e5e0c04909126f6
koresachin7 <koresachin7@gmail.com>
2021-12-08 11:49:15 +0530

    Added : - Use Players class To adding Multiple Player

--- snakeandladder.py
+++ snakeandladder.py
@@ -1,12 +1,13 @@
 import random
 import sys
+from logging import *
 
-MAX_VAL = 100
+MAX_VAL = 50
 # snake takes you down from 'key' to 'value'
-snakes = {8: 4, 18: 1, 26: 10, 39: 5, 51: 6, 54: 36, 60: 23, 90: 48, 92: 25, 97: 87, 99: 63}
+snakes = {8: 4, 18: 1, 26: 10, 39: 5}
 
 # ladder takes you up from 'key' to 'value'
-ladders = {3: 20, 6: 14, 11: 28, 15: 34, 17: 74, 22: 37, 38: 59, 49: 67, 57: 76, 61: 78, 73: 86, 81: 98, 88: 91}
+ladders = {3: 20, 6: 14, 11: 28, 15: 34, 17: 74, 22: 37}
 
 
 def welcome_msg():
@@ -17,7 +18,6 @@ def welcome_msg():
 
     msg = """
     Welcome to Snake and Ladder Game......
-
     Rules:
       1. initially both the players a
          take it in turns to roll the dice. 
@@ -29,24 +29,26 @@ def welcome_msg():
 
 
 class SnakeAndLadder:
+    def __init__(self):
+        self.list = []
 
-    def get_player_names(self):
-        """
-        Description:
-            this function assign players name
-        Return:
-            this function return players name
-        """
-        player1_name = None
-        while not player1_name:
-            player1_name = input("Please enter a valid name for first player: ")
-
-        player2_name = None
-        while not player2_name:
-            player2_name = input("Please enter a valid name for second player: ")
-
-        print("Match will be played between '" + player1_name + "' and '" + player2_name + "")
-        return player1_name, player2_name
+    # def get_player_names(self):
+    #     """
+    #     Description:
+    #         this function assign players name
+    #     Return:
+    #         this function return players name
+    #     """
+    #     player1_name = None
+    #     while not player1_name:
+    #         player1_name = input("Please enter a valid name for first player: ")
+    #
+    #     player2_name = None
+    #     while not player2_name:
+    #         player2_name = input("Please enter a valid name for second player: ")
+    #
+    #     print("Match will be played between '" + player1_name + "' and '" + player2_name + "")
+    #     return player1_name, player2_name
 
     def get_dice_value(self):
         """
@@ -73,7 +75,7 @@ class SnakeAndLadder:
         """
         print("\n" + player_name + " climbed the ladder from " + str(old_value) + " to " + str(current_value))
 
-    def snake_ladder(self, player_name, current_value, dice_value):
+    def snake_ladder(self, player, dice_value):
         """
         Description:
             this function for assign snake and ladder value
@@ -81,35 +83,35 @@ class SnakeAndLadder:
         Return:
             this function return fine value of player position
         """
-        old_value = current_value
-        current_value = current_value + dice_value
+        old_value = player.player1_current_position
+        player.player1_current_position = player.player1_current_position + dice_value
 
-        if current_value > MAX_VAL:
+        if player.player1_current_position > MAX_VAL:
             print("You need " + str(MAX_VAL - old_value) + " to win this game. Keep trying.")
             return old_value
 
-        print("\n" + player_name + " moved from " + str(old_value) + " to " + str(current_value))
-        if current_value in snakes:
-            final_value = snakes.get(current_value)
-            self.got_snake_bite(current_value, final_value, player_name)
+        print("\n" + player.player_name + " moved from " + str(old_value) + " to " + str(player.player1_current_position))
+        if player.player1_current_position in snakes:
+            final_value = snakes.get(player.player1_current_position)
+            self.got_snake_bite(player.player1_current_position, final_value, player.player_name)
 
-        elif current_value in ladders:
-            final_value = ladders.get(current_value)
-            self.got_ladder_jump(current_value, final_value, player_name)
+        elif player.player1_current_position in ladders:
+            final_value = ladders.get(player.player1_current_position)
+            self.got_ladder_jump(player.player1_current_position, final_value, player.player_name)
 
         else:
-            final_value = current_value
+            final_value = player.player1_current_position
 
         return final_value
 
-    def check_win(self, player_name, position):
+    def check_win(self, player):
         """
         Description:
             this function for check wining status of players
         """
-        if MAX_VAL == position:
-            print("That's it .." + player_name + " won the game.")
-            print("congratulations " + player_name)
+        if MAX_VAL == player.player1_current_position:
+            print("That's it .." + player.player_name + " won the game.")
+            print("congratulations " + player.player_name)
             sys.exit(1)
 
     def start(self):
@@ -118,30 +120,70 @@ class SnakeAndLadder:
             this function for start program calling multiple function
             and roll dice till one of the player win
         """
-        welcome_msg()
-        player1_name, player2_name = self.get_player_names()
 
-        player1_current_position = 0
+        # player1_name, player2_name = self.get_player_names()
+
         player2_current_position = 0
 
         while True:
-            input_1 = input("\n" + player1_name + ": "  "  enter to roll dice: ")
-            print("rolling dice...")
-            dice_value = self.get_dice_value()
-            print(player1_name + " moving....")
-            player1_current_position = self.snake_ladder(player1_name, player1_current_position, dice_value)
+            for player in self.list:
+                input_1 = input("\n" + player.player_name + ": "  "  enter to roll dice: ")
+                print("rolling dice...")
+                dice_value = self.get_dice_value()
+                print(player.player_name + " moving....")
+                player.player1_current_position = self.snake_ladder(player, dice_value)
 
-            self.check_win(player1_name, player1_current_position)
+                self.check_win(player)
 
-            input_2 = input("\n" + player2_name + ": "  "  enter to roll dice: ")
-            print("Rolling dice...")
-            dice_value = self.get_dice_value()
-            print(player2_name + " moving....")
-            player2_current_position = self.snake_ladder(player2_name, player2_current_position, dice_value)
+                # input_2 = input("\n" + player2_name + ": "  "  enter to roll dice: ")
+                # print("Rolling dice...")
+                # dice_value = self.get_dice_value()
+                # print(player2_name + " moving....")
+                # player2_current_position = self.snake_ladder(player2_name, player2_current_position, dice_value)
+                #
+                # self.check_win(player2_name, player2_current_position)
 
-            self.check_win(player2_name, player2_current_position)
+
+class Players:
+
+    def __init__(self, player_name):
+        self.player_name = player_name
+        self.player1_current_position = 0
+    #
+    # def get_player_names(self):
+    #     """
+    #     Description:
+    #         this function assign players name
+    #     Return:
+    #         this function return players name
+    #     """
+    #     self.player_name = None
+    #     while not self.player_name:
+    #         self.player_name = input("Please enter a valid name  player: ")
+    #     return self.player_name
 
 
 if __name__ == '__main__':
     snake_and_ladder = SnakeAndLadder()
+    LOG_FORMAT = '{lineno} *** {name} *** {asctime} *** {message}'
+    basicConfig(filename='logfile.log', level=DEBUG, style='{', format=LOG_FORMAT)
+
+    welcome_msg()
+    try:
+        player_num = int(input("Enter Player Quantity : --"))
+        if player_num > 1:
+            for player in range(player_num):
+                player_name = input("Please enter a valid name  player: ")
+                player_obj = Players(player_name)
+                # player_obj.get_player_names()
+                snake_and_ladder.list.append(player_obj)
+                print(snake_and_ladder.list)
+        else:
+            print("player is always greater than One please enter again....")
+            warning("player is always greater than One please enter again....")
+
+    except Exception:
+        print("Please Enter Number Only...")
+        error("Please Enter Number Only...")
+
     snake_and_ladder.start()
